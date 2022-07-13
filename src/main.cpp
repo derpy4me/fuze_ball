a4e0053083b1953ecca4be5e2f36f1a1a444481a
Tom Scott <tscott@stratanetworks.com>
2022-07-13 16:45:09 -0600

    implemented websockets and began reading break beams. Debouncing and lastState don't work

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,39 @@
 #include <Arduino.h>
+#include <WebSocketsClient.h>
+#include <WiFi.h>
 
 struct Button
 {
-  const int PIN;
+  int PIN;
   int numKeyPresses;
   bool currentState;
+  bool lastState;
   int lastPushTime;
-  int timeGap;
 };
 
-const int DEBOUNCE_TIME = 50; // Debounce time in milliseconds
+struct Beam
+{
+  int PIN;
+  bool currentState;
+  bool lastState;
+  int lastPushTime;
+};
+
+const char *ssid = "therealslimshady";
+const char *password = "pleasestandup";
+
+WebSocketsClient socket;
+
+#define USE_SERIAL Serial
+const int DEBOUNCE_TIME = 1000; // Debounce time in milliseconds
 
 int latchPin = 12;                         // Pin connected to ST_CP of 74HC595
 int clockPin = 13;                         // Pin connected to SH_CP of 74HC595
 int dataPin = 15;                          // Pin connected to DS of 74HC595
-Button greenButton = {18, 0, false, 0, 0}; // Pin connected to reset/start game button
-Button redButton = {5, 0, false, 0, 0};    // Pin connected to point undo button
-int homeBeam = 16;                         // Pin connected to home side break beam
-int awayBeam = 17;                         // Pin connected to away side break beam
+Button greenButton = {18, 0, false, false, 0}; // Pin connected to reset/start game button
+Button redButton = {5, 0, false, false, 0};    // Pin connected to point undo button
+Beam homeBeam = {16, false, false, 0};
+Beam awayBeam = {17, false, false, 0};
 
 // Define the encoding of characters 0-F of the common-anode 7-segment Display
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, b, C, d, E, F
@@ -25,24 +41,93 @@ int awayBeam = 17;                         // Pin connected to away side break b
 // byte num[] = {0xff, 0x7f, 0xbf, 0xdf, 0xef, 0xf7, 0xfb, 0xfd, 0xfe};
 byte num[] = {0xfe, 0xdf, 0xef, 0xf7, 0xfb, 0xfd};
 
-bool debounceButton(Button button) {
+Button debounceButton(Button button)
+{
   int now = millis();
-  button.timeGap = now - button.lastPushTime;
+  int timeGap = now - button.lastPushTime;
 
   bool state = digitalRead(button.PIN);
 
-  Serial.println();
-
-  if (button.timeGap > DEBOUNCE_TIME) {
-    if (!state) {
+  if (timeGap > DEBOUNCE_TIME)
+  {
+    if (!state)
+    {
       button.currentState = true;
-    } else {
+    }
+    else
+    {
       button.currentState = false;
     }
   }
 
+  button.lastState = button.currentState;
   button.lastPushTime = now;
-  return button.currentState;
+  return button;
+}
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+
+  USE_SERIAL.println(length);
+  USE_SERIAL.println(type);
+
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    USE_SERIAL.printf("[WSc] Disconnected!\n");
+    break;
+  case WStype_CONNECTED:
+    USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+
+    // send message to server when Connected
+    socket.sendTXT("Connected");
+    break;
+  case WStype_TEXT:
+    USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+
+    // send message to server
+    // webSocket.sendTXT("message here");
+    break;
+  case WStype_BIN:
+    USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+    // hexdump(payload, length);
+
+    // send data to server
+    // webSocket.sendBIN(payload, length)
+    break;
+  case WStype_ERROR:
+  case WStype_PING:
+  case WStype_PONG:
+  case WStype_FRAGMENT_TEXT_START:
+  case WStype_FRAGMENT_BIN_START:
+  case WStype_FRAGMENT:
+  case WStype_FRAGMENT_FIN:
+    break;
+  }
+}
+
+Beam debounceBeam(Beam beam)
+{
+  int now = millis();
+  int timeGap = now - beam.lastPushTime;
+
+  bool state = digitalRead(beam.PIN);
+
+  if (timeGap > DEBOUNCE_TIME)
+  {
+    if (!state)
+    {
+      beam.currentState = true;
+    }
+    else
+    {
+      beam.currentState = false;
+    }
+  }
+
+  beam.lastState = beam.currentState;
+  beam.lastPushTime = now;
+  return beam;
 }
 
 void setup()
@@ -55,10 +140,36 @@ void setup()
   // Set input pins
   pinMode(redButton.PIN, INPUT_PULLUP);
   pinMode(greenButton.PIN, INPUT_PULLUP);
-  pinMode(homeBeam, INPUT);
-  pinMode(awayBeam, INPUT);
+  pinMode(homeBeam.PIN, INPUT_PULLUP);
+  pinMode(awayBeam.PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
+
+  USE_SERIAL.println("Start Setup");
+
+  for (uint8_t t = 4; t > 0; t--)
+  {
+    USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+  WiFi.mode(WIFI_AP);
+
+  WiFi.begin(ssid, password);
+
+  // WiFi.disconnect();
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+  }
+
+  socket.begin("67.213.228.103", 8000, "/table/ws/Testing");
+
+  // event handler
+  socket.onEvent(webSocketEvent);
+
+  // try ever 5000 again if connection has failed
+  socket.setReconnectInterval(5000);
 }
 
 void loop()
@@ -75,17 +186,36 @@ void loop()
   //   delay(500);
   // }
 
-  redButton.currentState = debounceButton(redButton);
-  greenButton.currentState = debounceButton(greenButton);
+  redButton = debounceButton(redButton);
+  greenButton = debounceButton(greenButton);
 
-  Serial.printf("Green Button %u", greenButton.currentState);
-
-  if (redButton.currentState) {
+  if (redButton.currentState)
+  {
     Serial.println("Red button pressed");
   }
-  if (greenButton.currentState) {
+  if (greenButton.currentState)
+  {
     Serial.println("Green Button pressed");
   }
+
+  Serial.println(greenButton.currentState);
+  Serial.println(greenButton.lastState);
+
+  homeBeam = debounceBeam(homeBeam);
+  awayBeam = debounceBeam(awayBeam);
+
+
+  if (homeBeam.currentState && !homeBeam.lastState)
+  {
+    Serial.println("Home Beam Activated");
+  }
+
+  if (awayBeam.currentState && !awayBeam.lastState)
+  {
+    Serial.println("Away Beam Activated");
+  }
+
+  socket.loop();
   delay(100);
 }
 
